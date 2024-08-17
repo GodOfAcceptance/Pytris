202ef27c2e03c9c31cc519054d9794c57c2a6319
Bill Park <bill.park.cs@gmail.com>
2024-08-17 16:31:12 -0500

    Added a baseline training loop using stable baselines A2C

--- main.py
+++ main.py
@@ -39,15 +39,18 @@ class Main:
             if not self.gameIsRunning:
                 break;
             
-            done, info = self.env.reset()
+            obs, info = self.env.reset()
+            done = False
             
             while not done:
+                
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         done = True
                         self.gameIsRunning = False
                         break;
-                action = self.agent.getAction()
+                    
+                action = self.agent.predict(obs)[0]
                 obs, reward, terminated, truncated, info = self.env.step(action)
                 
                 if terminated or truncated:
@@ -56,7 +59,8 @@ class Main:
         
         self.gameIsRunning = False
         self.env.close()
-         
+        
+        
 
     def playHuman(self):
         self.env.reset()
@@ -185,62 +189,79 @@ if __name__ == '__main__':
     #main = Main(player="agent", agent=RandomAgent(), render_mode='human', sfx=True)
     #main.run()
     parser = argparse.ArgumentParser(description='Example')
-    parser.add_argument('--player', type=str, default='human', help='human or agent', required=True)
-    parser.add_argument('--agent', type=str, default='random', help='agent class', required=False)
-    parser.add_argument('--render_mode', type=str, default='human', help='human or rgb_array', required=True)
-    parser.add_argument('--sfx', type=bool, default=False, help='sound effects', required=False)
+    parser.add_argument('--player', type=str, default='human', help='human or agent', required=False)
+    parser.add_argument('--agent', type=str, default=None, help='agent class', required=False)
+    parser.add_argument('--render_mode', type=str, default='human', help='human or rgb_array', required=False)
+    parser.add_argument('--sfx', action='store_true', help='sound effects', required=False)
     parser.add_argument('--DAS', type=int, default=8, help='delayed auto shift in frames', required=False)
     parser.add_argument('--ARR', type=int, default=1, help='auto repeat rate in frames', required=False)
     parser.add_argument('--n_episodes', type=int, default=5, help='number of episodes', required=False)
+    parser.add_argument('--train', action='store_true', help='train agent', required=False)
     
     args = parser.parse_args()
-    
-    if not args.player in ["human", "agent"]:
-        print("unsupported player. Available: human, agent")
-        exit()
-        
-    if args.render_mode not in ["human", "rgb_array"]:
-        print("unsupported render_mode. Available: human, rgb_array")
-        exit()
-        
-    if args.player == "agent" and args.agent is None:
-        print("agent class is required")
-        exit()
-        
-    if args.render_mode == "rgb_array" and args.sfx:
-        print("sound effects are not supported in rgb_array mode")
-        exit()
-    
-    if args.player == "human" and args.render_mode == "rgb_array":
-        print("human player is not supported in rgb_array mode")
-        exit()
-    
-    if args.DAS < 0 or args.ARR < 0:
-        print("DAS and ARR values must be positive")
-        exit()
-        
-    if args.n_episodes < 1:
-        print("n_episodes must be greater than 0")
-        exit()
-        
+    if args.train:
+        from stable_baselines3 import A2C
+        from stable_baselines3.common.env_util import make_vec_env
+        from stable_baselines3.common.vec_env import SubprocVecEnv
 
-    agent = None
-    
-    match args.agent:
-        case "random":
-            agent = RandomAgent()
-        case _:
+        # Parallel environments
+        vec_env = make_vec_env(tetris_env.TetrisEnv, n_envs=4, vec_env_cls=SubprocVecEnv)
+
+        model = A2C("MultiInputPolicy", vec_env, verbose=1, device="cpu")
+        model.learn(total_timesteps=25000)
+        model.save("a2ctetris")
+            
+    else:
+        if not args.player in ["human", "agent"]:
+            print("unsupported player. Available: human, agent")
             exit()
-    
-    
-    main = Main(player=args.player, 
-                agent=agent, 
-                render_mode=args.render_mode,
-                nEpisodes=args.n_episodes, 
-                sfx=args.sfx, 
-                DAS=args.DAS, 
-                ARR=args.ARR,
-                )
-    main.run()
+            
+        if args.render_mode not in ["human", "rgb_array"]:
+            print("unsupported render_mode. Available: human, rgb_array")
+            exit()
+            
+        if args.player == "agent" and args.agent is None:
+            print("agent class is required")
+            exit()
+            
+        if args.render_mode == "rgb_array" and args.sfx:
+            print("sound effects are not supported in rgb_array mode")
+            exit()
+        
+        if args.player == "human" and args.render_mode == "rgb_array":
+            print("human player is not supported in rgb_array mode")
+            exit()
+        
+        if args.DAS < 0 or args.ARR < 0:
+            print("DAS and ARR values must be positive")
+            exit()
+            
+        if args.n_episodes < 1:
+            print("n_episodes must be greater than 0")
+            exit()
+            
+
+        agent = None
+        if args.agent is not None:
+            match args.agent:
+                case "random":
+                    agent = RandomAgent()
+                case "a2c":
+                    from stable_baselines3 import A2C
+                    agent = A2C.load("a2ctetris")
+                case _:
+                    print("unsupported agent")
+                    exit()
+        
+        
+        main = Main(player=args.player, 
+                    agent=agent, 
+                    render_mode=args.render_mode,
+                    nEpisodes=args.n_episodes, 
+                    sfx=args.sfx, 
+                    DAS=args.DAS, 
+                    ARR=args.ARR,
+                    )
+        main.run()
     
     
