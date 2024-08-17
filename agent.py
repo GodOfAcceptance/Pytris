8670db3b0d9dea77f2716d53e9c662583500814b
Bill Park <bill.park.cs@gmail.com>
2024-08-17 16:30:43 -0500

    redefined return value of random agent

--- agent.py
+++ agent.py
@@ -1,17 +1,9 @@
 import tetris_env
 
-env = tetris_env.TetrisEnv(render_mode=None)
+env = tetris_env.TetrisEnv(render_mode="human")
 class RandomAgent:
     def __init__(self):
         pass
     
-    def getAction(self):
-        rawAction = env.action_space.sample()
-        trueAction = [0,0,0,0]
-        trueAction[0] = (2 * rawAction[0] - 3) * (rawAction[0] != 0)
-        trueAction[1] = (2 * rawAction[1] - 3) * (rawAction[1] != 0)
-        trueAction[2] = rawAction[2]
-        trueAction[3] = rawAction[3]
-        return trueAction
-
-    
+    def predict(self, obs):
+        return [env.action_space.sample()]
